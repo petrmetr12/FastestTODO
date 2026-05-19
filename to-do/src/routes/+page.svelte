7f2f0e95d9dc969b38111cb52454f0b6a5715622
petrmetr12 <peterhrmel70@gmail.com>
2026-05-19 13:58:51 +0200

    pridani vice tailwindu

--- to-do/src/routes/+page.svelte
+++ to-do/src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
-    /**let ti
-     * {
-     *  "text": "Vynést koš",
-     *  "completed": false/true
-     * }
-     */
-
-    let todos = $state<string[]>([]);
-    let doneTasks = $state<string[]>([]);
+    let todos = $state<{text: string, done: boolean}[]>([]);
     let newTodo = $state('');
 
     function addTodo() {
         if (newTodo.trim() !== '') {
-            todos.push(newTodo);
+            todos.push({ text: newTodo.trim(), done: false });
             newTodo = '';
         } 
     }
@@ -21,24 +13,30 @@
 <div class="max-w-100 p-5 border border-gray-400 rounded-4xl bg-gray-500 mx-auto my-12">
     <h1><strong>To-Do List</strong></h1>
     <input type="text" bind:value={newTodo} placeholder="Add a new task" />
-    <button onclick={addTodo} class="button">Add</button>
-    <button onclick={() => { todos = []; doneTasks = []; }} class="button-each bg-red-300!">Clear All</button>
+    <button onclick={addTodo} class="bg-yellow-600 hover:bg-yellow-700 focus:outline-1 
+                focus:outline-offset-2 focus:outline-yellow-600 active:bg-yellow-800
+                pt-2 pb-2">Add</button>
+    <button onclick={() => { todos = []; }} class=" bg-red-600 hover:bg-red-700 focus:outline-1 focus:outline-offset-2
+                                                     focus:outline-red-600 active:bg-red-800">Clear All
+    </button>
 
     <ul>
         {#each todos as todo}
             <li>
                 <input 
                     type="checkbox" 
-                    value={todo} 
-                    bind:group={doneTasks} 
+                    bind:checked={todo.done} 
                 />
-                <span style={doneTasks.includes(todo) ? 'text-decoration: line-through;' : ''}>
-                    {todo}
+                <span style={todo.done ? 'text-decoration: line-through;' : ''}>
+                    {todo.text}
                 </span>
                 <button onclick={() => {
                     todos = todos.filter(t => t !== todo);
-                    doneTasks = doneTasks.filter(t => t !== todo);
-                }} class="button-each">Delete</button>
+                }} class="bg-red-600 hover:bg-red-700 focus:outline-1 
+                focus:outline-offset-2 focus:outline-red-600 active:bg-red-800
+                pt-2 pb-2 br"
+        
+                >Delete</button>
             </li>
         {/each}
     </ul>
@@ -46,16 +44,13 @@
 
 <style>
     button {
-        background-color: #007BFF;
         color: white;
         border: none;
         border-radius: 20px;
         padding: 10px 20px;
         cursor: pointer;
     }
-    button:hover {        
-        background-color: #0056b3;
-    }
+    
     .container {
         max-width: 400px;
         margin: 50px auto;
